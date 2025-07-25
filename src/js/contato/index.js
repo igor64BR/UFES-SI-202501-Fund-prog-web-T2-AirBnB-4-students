00aecd7193c5d49b124306c97c77a54b9b204a86
Baiocco-iALL <igor.rodrigues@industriall.ai>
2025-07-25 10:11:14 -0300

    refact: remove javascript para contato.html

--- src/js/contato/index.js
+++
@@ -1,7 +0,0 @@
-const sendEmail = (emailSendingEvent) => {
-    emailSendingEvent.preventDefault();
-    const form = emailSendingEvent.target;
-    const { email, assunto, mensagem } = form.elements;
-
-    window.location.href = `mailto:${email.value}?subject=${encodeURIComponent(assunto.value)}&body=${encodeURIComponent(mensagem.value)}`;
-}
